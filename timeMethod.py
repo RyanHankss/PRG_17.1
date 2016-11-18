9e1a08a2d5d9a622f643e342216d865ae1074295
RyanHankss <rhanks@mchenry.edu>
2016-11-18 14:34:13 -0600

    Update timeMethod.py

--- timeMethod.py
+++ timeMethod.py
@@ -10,3 +10,9 @@ t.minute = 34
 t.second = 33
 
 Time.time_int(t)
+
+
+# it is probably not appropriate to rewrite int_to_time as a method;
+# what object you would invoke it on?
+
+# Because int_to_time already has the object within the function.
